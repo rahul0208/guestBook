00ff188abd51d5e491a30ec8f98130452dbb1653
Rahul Sharma <rahul0208@gmail.com>
2021-10-27 17:20:57 +0530

    Added AAD authntication

--- src/main/java/org/sample/guestbook/BookController.java
+++ src/main/java/org/sample/guestbook/BookController.java
@@ -1,17 +1,26 @@
 package org.sample.guestbook;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
 
 @Controller
 public class BookController {
     private BookEntryDTO savedValue;
 
     @GetMapping({"/"})
-    public String loadBookEntry(Model model) {
-        model.addAttribute("bookentry", new BookEntryDTO());
+    public String loadBookEntry(Model model, @AuthenticationPrincipal OidcUser principal) {
+        BookEntryDTO dto = new BookEntryDTO();
+        dto.setName(principal.getName());
+        model.addAttribute("bookentry", dto);
         model.addAttribute("latest", savedValue);
         return "bookentry";
     }
